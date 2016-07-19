306203aa90ad174722b048f67fc275a5d0b758c5
Austin Dean <austin.j.dean@lmco.com>
2016-07-19 16:15:16 -0400

    literally just more comments

--- lns.sh
+++ lns.sh
@@ -4,7 +4,7 @@
 # $2 = link location
 
 if [[ -d $2 ]]; then # directory exists
-	# place in directory with same name as target executable
+	# create symlink in directory with same name as target executable
 	pants=${1##*/}
 	# thanks: http://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
 	ln -s $(readlink -f $1) $(readlink -f $2)/$pants
@@ -13,6 +13,6 @@ elif [[ -a $2 ]]; then # file exists
 	echo Target file exists - exiting to prevent overwrite.
 	exit 0
 else # file doesn't exist
-	# create symlink with given name since it's safe
+	# create symlink with given path/name since it's safe
 	ln -s $(readlink -f $1) $(readlink -f $2)
 fi
