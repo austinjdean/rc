8f5a84d97e7f35631a083b8120a258f91ba76afa
Austin Dean <austinjdean@gmail.com>
2017-02-20 13:01:23 -0500

    Commit from asis alias

--- gen.sh
+++ gen.sh
@@ -1,15 +1,23 @@
 #/bin/bash
 
+# if [ -z "$1" ]; then
+# 	echo "gen e for ethernet, gen w for wireless"
+# 	exit 1
+# else
+# 	if [ $1 = "e" ]; then
+# 		echo scp -r $(whoami)@$(ifconfig ens33 | perl -nle 's/dr:(\S+)/print $1/e'):$(pwd)/* .
+# 	elif [ $1 = "w" ]; then
+# 		echo scp -r $(whoami)@$(ifconfig wlan0 | perl -nle 's/dr:(\S+)/print $1/e'):$(pwd)/* .
+# 	else
+# 		echo "gen e for ethernet, gen w for wireless"
+# 		exit 1
+# 	fi
+# fi
+
 if [ -z "$1" ]; then
-	echo "gen e for ethernet, gen w for wireless"
+	echo "usage: gen <interface>"
+	echo "(ifconfig for list of interfaces)"
 	exit 1
 else
-	if [ $1 = "e" ]; then
-		echo scp -r $(whoami)@$(ifconfig ens33 | perl -nle 's/dr:(\S+)/print $1/e'):$(pwd)/* .
-	elif [ $1 = "w" ]; then
-		echo scp -r $(whoami)@$(ifconfig wlan0 | perl -nle 's/dr:(\S+)/print $1/e'):$(pwd)/* .
-	else
-		echo "gen e for ethernet, gen w for wireless"
-		exit 1
-	fi
+	echo scp -r $(whoami)@$(ifconfig $1 | perl -nle 's/dr:(\S+)/print $1/e'):$(pwd)/* .
 fi
